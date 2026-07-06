60b5ff3a507f157ddd1fd01d7d5f38cc2e2700e4
Mark Kapapa <markkapapa15@gmail.com>
2026-07-06 08:03:31 -0400

    Added a nice looking main scene

--- gym_bro.gd
+++ gym_bro.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 800.0
+const JUMP_VELOCITY = -800.0
 
 @onready var skeleton = $Bro 
 
@@ -12,18 +12,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, 100)
 		skeleton.scale.x = sign(direction)
 		$AnimationPlayer.speed_scale = 1.5
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 50)
 		$AnimationPlayer.speed_scale = 1.0
 
 	if direction != 0:
